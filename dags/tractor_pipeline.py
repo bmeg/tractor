0629dcb9ac021538e68f970cde92595d07c0cc58
Kyle Ellrott <kellrott@gmail.com>
2025-01-31 15:54:29 -0800

    Starting to prototyoe a modular build

---
+++ dags/tractor_pipeline.py
@@ -0,0 +1,20 @@
+
+
+import os
+import tractor
+from datetime import datetime
+from airflow.models import Variable
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+PROJECT_DIR = Variable.get("project_dir")
+logger.info(f"Tractor-Project {PROJECT_DIR}")
+
+
+if os.path.exists(PROJECT_DIR):
+    with DAG(dag_id="tractor-build", schedule=None, start_date=datetime(2022, 3, 4)) as dag:
+        tractor.build(PROJECT_DIR, dag)
