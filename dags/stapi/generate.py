3ac6cfaf5abcf4679e6ccf025ac5f4bcfc45b427
Brian Walsh <brian@bwalsh.com>
2025-02-07 10:02:15 -0800

    warn if no config

--- dags/stapi/generate.py
+++ dags/stapi/generate.py
@@ -2,11 +2,16 @@ import logging
 import pathlib
 
 from tractor.mkdag import generate
+log = logging.getLogger(__name__)
 
 configs = pathlib.Path("/opt/airflow/projects/stapi")
+if not configs.exists():
+    log.warning(f"No configs found in {configs}")
+    exit()
+
 generated_dags = []
 for config in configs.glob("stapi-*.yaml"):
     generated_dags.extend(generate(str(config)))
 
 for dag in generated_dags:
-    logging.info(f"Generated DAG: {dag}")
+    log.info(f"Generated DAG: {dag}")
