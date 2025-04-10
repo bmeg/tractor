1869fc869f74d55d0976f4e40498ea128ee42856
Brian Walsh <brian@bwalsh.com>
2025-04-10 14:57:09 -0700

    formatting

--- tests/unit/etl_dsl/test_dsl.py
+++ tests/unit/etl_dsl/test_dsl.py
@@ -95,6 +95,7 @@ def test_load_minimal_etl_to_tes(minimal_etl_project):
     assert tes_task.name == our_task.name
     assert len(tes_task.executors) == 1
     import shlex
+
     assert tes_task.executors[0].command == shlex.split(our_command.command)
 
     # TODO - make this more robust
